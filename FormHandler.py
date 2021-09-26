9b957fae120c7c2ac5e45c7efca4845fa662d6d1
artemkyz89 <artemkyz89@gmail.com>
2021-09-26 20:56:21 +0600

    refactoring

--- FormHandler.py
+++ FormHandler.py
@@ -31,6 +31,7 @@ def action13():
         tid = response['tid'].split(' ')
         product_t = response['product_type']
 
+        # Количество TID должно быть равно количеству GTIN и наоборот.
         if len(gtin) != len(tid):
             if len(gtin) < len(tid):
                 return render_template('error.html', reason='Количество GTIN меньше TID')
@@ -57,119 +58,42 @@ def action13():
 
             # Сложение полученных результатов в SGTIN96
             d[gtin] = [tid, '00110000001011' + gln_binary + '0' + product_binary + tid_bin]
-        print(d)
 
-        # Здесь идёт проверка условий для комиссионной, новой или возвратной продукции
-        if product_t == '1':
-            now = datetime.strftime(datetime.now(), "%Y-%m-%dT%H:%M:%SZ")
-            xsd = 'http://www.w3.org/2001/XMLSchema'
-            xsi = 'http://www.w3.org/2001/XMLSchema-instance'
-            page = lxml.builder.ElementMaker(
-                nsmap={
-                    "xsd": xsd,
-                    "xsi": xsi
-                }
-            )
-            root = page.query
-            page = root()
-            page.attrib['version'] = '2.41'
-            page.attrib['{{{pre}}}noNamespaceSchemaLocation'.format(pre=xsi)] = '..\\xsd_new1\\query.xsd'
-            unify_self_signs = etree.SubElement(page, "unify_self_signs")
-            unify_self_signs.attrib['action_id'] = '20'
-            sender_gln = etree.SubElement(unify_self_signs, "sender_gln")
-            sender_gln.text = gln
-            unify_date = etree.SubElement(unify_self_signs, "unify_date")
-            unify_date.text = now
-            unifies = etree.SubElement(unify_self_signs, "unifies")
-            for key in d.keys():
-                by_gtin = etree.SubElement(unifies, "by_gtin")
-                sign_gtin = etree.SubElement(by_gtin, "sign_gtin")
-                sign_gtin.text = key
-                union = etree.SubElement(by_gtin, "union")
-                gs1_sgtin = etree.SubElement(union, "gs1_sgtin")
-                gs1_sgtin.text = d.get(key)[1]
-                sign_num = etree.SubElement(union, "sign_num")
-                sign_num.text = d.get(key)[0]
-                product_type = etree.SubElement(union, "product_type")
-                product_type.text = str(product_t)
-            doc = etree.ElementTree(page)
-            doc.write(f"{UPLOAD_FOLDER}/{gln}.xml", pretty_print=True)
-            return send_from_directory(UPLOAD_FOLDER,
-                                       f'{gln}.xml', as_attachment=True)
-
-        elif product_t == '0':
-            now = datetime.strftime(datetime.now(), "%Y-%m-%dT%H:%M:%SZ")
-            xsd = 'http://www.w3.org/2001/XMLSchema'
-            xsi = 'http://www.w3.org/2001/XMLSchema-instance'
-            page = lxml.builder.ElementMaker(
-                nsmap={
-                    "xsd": xsd,
-                    "xsi": xsi
-                }
-            )
-            root = page.query
-            page = root()
-            page.attrib['version'] = '2.41'
-            page.attrib['{{{pre}}}noNamespaceSchemaLocation'.format(pre=xsi)] = '..\\xsd_new1\\query.xsd'
-            unify_self_signs = etree.SubElement(page, "unify_self_signs")
-            unify_self_signs.attrib['action_id'] = '20'
-            sender_gln = etree.SubElement(unify_self_signs, "sender_gln")
-            sender_gln.text = gln
-            unify_date = etree.SubElement(unify_self_signs, "unify_date")
-            unify_date.text = now
-            unifies = etree.SubElement(unify_self_signs, "unifies")
-            for key in d.keys():
-                by_gtin = etree.SubElement(unifies, "by_gtin")
-                sign_gtin = etree.SubElement(by_gtin, "sign_gtin")
-                sign_gtin.text = key
-                union = etree.SubElement(by_gtin, "union")
-                gs1_sgtin = etree.SubElement(union, "gs1_sgtin")
-                gs1_sgtin.text = d.get(key)[1]
-                sign_num = etree.SubElement(union, "sign_num")
-                sign_num.text = d.get(key)[0]
-                product_type = etree.SubElement(union, "product_type")
-                product_type.text = str(product_t)
-            doc = etree.ElementTree(page)
-            doc.write(f"{UPLOAD_FOLDER}/{gln}.xml", pretty_print=True)
-            return send_from_directory(UPLOAD_FOLDER,
-                                       f'{gln}.xml', as_attachment=True)
-
-        elif product_t == '2':
-            now = datetime.strftime(datetime.now(), "%Y-%m-%dT%H:%M:%SZ")
-            xsd = 'http://www.w3.org/2001/XMLSchema'
-            xsi = 'http://www.w3.org/2001/XMLSchema-instance'
-            page = lxml.builder.ElementMaker(
-                nsmap={
-                    "xsd": xsd,
-                    "xsi": xsi
-                }
-            )
-            root = page.query
-            page = root()
-            page.attrib['version'] = '2.41'
-            page.attrib['{{{pre}}}noNamespaceSchemaLocation'.format(pre=xsi)] = '..\\xsd_new1\\query.xsd'
-            unify_self_signs = etree.SubElement(page, "unify_self_signs")
-            unify_self_signs.attrib['action_id'] = '20'
-            sender_gln = etree.SubElement(unify_self_signs, "sender_gln")
-            sender_gln.text = gln
-            unify_date = etree.SubElement(unify_self_signs, "unify_date")
-            unify_date.text = now
-            unifies = etree.SubElement(unify_self_signs, "unifies")
-            for key in d.keys():
-                by_gtin = etree.SubElement(unifies, "by_gtin")
-                sign_gtin = etree.SubElement(by_gtin, "sign_gtin")
-                sign_gtin.text = key
-                union = etree.SubElement(by_gtin, "union")
-                gs1_sgtin = etree.SubElement(union, "gs1_sgtin")
-                gs1_sgtin.text = d.get(key)[1]
-                sign_num = etree.SubElement(union, "sign_num")
-                sign_num.text = d.get(key)[0]
-                product_type = etree.SubElement(union, "product_type")
-                product_type.text = str(product_t)
-            doc = etree.ElementTree(page)
-            doc.write(f"{UPLOAD_FOLDER}/{gln}.xml", pretty_print=True)
-            return send_from_directory(UPLOAD_FOLDER,
-                                       f'{gln}.xml', as_attachment=True)
+        now = datetime.strftime(datetime.now(), "%Y-%m-%dT%H:%M:%SZ")
+        xsd = 'http://www.w3.org/2001/XMLSchema'
+        xsi = 'http://www.w3.org/2001/XMLSchema-instance'
+        page = lxml.builder.ElementMaker(
+            nsmap={
+                "xsd": xsd,
+                "xsi": xsi
+            }
+        )
+        root = page.query
+        page = root()
+        page.attrib['version'] = '2.41'
+        page.attrib['{{{pre}}}noNamespaceSchemaLocation'.format(pre=xsi)] = '..\\xsd_new1\\query.xsd'
+        unify_self_signs = etree.SubElement(page, "unify_self_signs")
+        unify_self_signs.attrib['action_id'] = '20'
+        sender_gln = etree.SubElement(unify_self_signs, "sender_gln")
+        sender_gln.text = gln
+        unify_date = etree.SubElement(unify_self_signs, "unify_date")
+        unify_date.text = now
+        unifies = etree.SubElement(unify_self_signs, "unifies")
+        for key in d.keys():
+            by_gtin = etree.SubElement(unifies, "by_gtin")
+            sign_gtin = etree.SubElement(by_gtin, "sign_gtin")
+            sign_gtin.text = key
+            union = etree.SubElement(by_gtin, "union")
+            gs1_sgtin = etree.SubElement(union, "gs1_sgtin")
+            gs1_sgtin.text = d.get(key)[1]
+            sign_num = etree.SubElement(union, "sign_num")
+            sign_num.text = d.get(key)[0]
+            product_type = etree.SubElement(union, "product_type")
+            product_type.text = str(product_t)
+        doc = etree.ElementTree(page)
+        doc.write(f"{UPLOAD_FOLDER}/{gln}.xml", pretty_print=True)
+        return send_from_directory(UPLOAD_FOLDER,
+                                   f'{gln}.xml', as_attachment=True)
 
     except ValueError as error:
         return render_template('error.html', reason=error)
