3326cc82df04b156dd370ebc04bada005fcece7b
artemkyz89 <artemkyz89@gmail.com>
2021-09-27 21:29:16 +0600

    add email filed

--- FormHandler.py
+++ FormHandler.py
@@ -21,6 +21,8 @@ def hello_world():
 def action13():
     try:
         response = dict(request.form)
+        email = response['email']
+        print(email)
         gln = response['gln']
         gtin = response['gtin'].split(' ')
         tid = response['tid'].split(' ')
